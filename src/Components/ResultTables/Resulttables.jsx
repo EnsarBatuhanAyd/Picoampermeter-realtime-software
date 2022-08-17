516672f90c4256783985e5ef406966502fc52844
EnsarBatuhanAyd <ensarbatuhanayd@gmail.com>
2022-08-17 11:45:36 +0300

    exporting json file

--- src/Components/ResultTables/Resulttables.jsx
+++ src/Components/ResultTables/Resulttables.jsx
@@ -5,18 +5,19 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const Resulttables = () => {
-  const [download, setDownload] = useState([]);
-  const downloadLink = useRef();
-
-  const [Data0, setData0] = useState("No Data For View");
-  const [Data1, setData1] = useState("No Data For View");
-  const [Data2, setData2] = useState("No Data For View");
-  const [Data3, setData3] = useState("No Data For View");
-  const [Data4, setData4] = useState("No Data For View");
-  const [Data5, setData5] = useState("No Data For View");
-  const [Data6, setData6] = useState("No Data For View");
-  const [Data7, setData7] = useState("No Data For View");
-  const [Data8, setData8] = useState("No Data For View");
+  const [DataName0, setDataName0] = useState("No Data For View");
+  const [Data0, setData0] = useState("");
+  const [DataName1, setDataName1] = useState("No Data For View");
+  const [Data1, setData1] = useState("");
+  const [DataName2, setDataName2] = useState("No Data For View");
+  const [Data2, setData2] = useState("");
+  const [DataName3, setDataName3] = useState("No Data For View");
+  const [Data3, setData3] = useState("");
+  const [DataName4, setDataName4] = useState("No Data For View");
+  const [DataName5, setDataName5] = useState("No Data For View");
+  const [DataName6, setDataName6] = useState("No Data For View");
+  const [DataName7, setDataName7] = useState("No Data For View");
+  const [DataName8, setDataName8] = useState("No Data For View");
 
   const db = firebase.firestore();
 
@@ -28,27 +29,108 @@ const Resulttables = () => {
     const dataresult = String(data[0].id);
     console.log(data);
     console.log(dataresult);
-    setData0(data[0].id);
-    setData1(data[1].id);
-    setData2(data[2].id);
+    setDataName0(data[0].id);
+    setDataName1(data[1].id);
   });
-  const downloadMyCollection = async () => {
-    const myCollection = db
-      .collection("Measurement")
-      .doc(Data0)// I will send doc id
-      .collection("data");
-    myCollection.onSnapshot(function (snapshot) {
-      const data = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setDownload(data);
-    });
-  };
-  useEffect(() => {
-    if (!!download.length) downloadLink.current.click();
-  }, [download]);
+  const exportDataName0 = () => {
+    console.log(DataName0 + " data downloading...");
+    const doc_ref = db.collection("Measurement");
+    const doc_ref2 = doc_ref.doc(DataName0);
+    doc_ref2
+      .collection("data")
+      .orderBy("Time", "asc")
+      .onSnapshot(function (snapshot) {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setData0(data);
+        console.log(data);
+      });
 
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(Data0)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+
+    link.click();
+  };
+  const exportDataName1 = () => {
+    console.log(DataName1 + " data downloading...");
+    const doc_ref = db.collection("Measurement");
+    const doc_ref2 = doc_ref.doc(DataName1);
+    doc_ref2
+      .collection("data")
+      .orderBy("Time", "asc")
+      .onSnapshot(function (snapshot) {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setData1(data);
+        console.log(data);
+      });
+
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(Data1)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+
+    link.click();
+  };
+  const exportDataName2 = () => {
+    console.log(DataName2 + " data downloading...");
+    const doc_ref = db.collection("Measurement");
+    const doc_ref2 = doc_ref.doc(DataName2);
+    doc_ref2
+      .collection("data")
+      .orderBy("Time", "asc")
+      .onSnapshot(function (snapshot) {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setData2(data);
+        console.log(data);
+      });
+
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(Data2)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+
+    link.click();
+  };const exportDataName3 = () => {
+    console.log(DataName3 + " data downloading...");
+    const doc_ref = db.collection("Measurement");
+    const doc_ref2 = doc_ref.doc(DataName3);
+    doc_ref2
+      .collection("data")
+      .orderBy("Time", "asc")
+      .onSnapshot(function (snapshot) {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setData3(data);
+        console.log(data);
+      });
+
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(Data3)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+
+    link.click();
+  };
   return (
     <div className="bg-Resulttables">
       <div className="Table-titles-area">
@@ -61,69 +143,62 @@ const Resulttables = () => {
       </div>
       <div className="Table-data-area">
         <ul className="Table-datas">
-          <li>{Data0}</li>
-          <li className="download-image">
-            <button className="downloadButton" onClick={downloadMyCollection}>
-              Download Responses
-            </button>
+          <li>{DataName0}</li>
 
-            <a
-              href={`data:text/json;charset=utf-8,${encodeURIComponent(
-                JSON.stringify(download)
-              )}`}
-              download="export.json"
-              className="hidden"
-              ref={downloadLink}
-            >
-              isHidden
-            </a>
-          </li>
+          <button className="downloadButton" onClick={exportDataName0}>
+            <li className="download-image"> </li>
+          </button>
         </ul>
         <ul className="Table-datas">
-          <li>{Data1}</li>
-          <li className="download-image"> <button className="downloadButton" onClick={downloadMyCollection}>
-              Download Responses
-            </button>
-
-            <a
-              href={`data:text/json;charset=utf-8,${encodeURIComponent(
-                JSON.stringify(download)
-              )}`}
-              download="export.json"
-              className="hidden"
-              ref={downloadLink}
-            >
-              isHidden
-            </a></li>
+          <li>{DataName1}</li>
+          <button className="downloadButton" onClick={exportDataName1}>
+            <li className="download-image"> </li>{" "}
+          </button>
         </ul>
+        
         <ul className="Table-datas">
-          <li>{Data2}</li>
-          <li className="download-image"></li>
-        </ul>
-        <ul className="Table-datas">
-          <li>{Data3}</li>
-          <li className="download-image"></li>
-        </ul>
-        <ul className="Table-datas">
-          <li>{Data4}</li>
-          <li className="download-image"></li>
-        </ul>
-        <ul className="Table-datas">
-          <li>{Data5}</li>
-          <li className="download-image"></li>
-        </ul>
-        <ul className="Table-datas">
-          <li>{Data6}</li>
-          <li className="download-image"></li>
-        </ul>
-        <ul className="Table-datas">
-          <li>{Data7}</li>
-          <li className="download-image"></li>
-        </ul>
-        <ul className="Table-datas">
-          <li>{Data8}</li>
-          <li className="download-image"></li>
-        </ul>
+           <li>{DataName2}</li>
+           <button className="downloadButton" onClick={exportDataName2}>
+             <li className="download-image"> </li>{" "}
+           </button>
+         </ul>
+         <ul className="Table-datas">
+           <li>{DataName3}</li>
+            <button className="downloadButton" onClick={exportDataName3}>
+             <li className="download-image"> </li>{" "}
+           </button>
+         </ul>
+        {/*
+        // <ul className="Table-datas">
+        //   <li>{DataName4}</li>
+        //    <button className="downloadButton" onClick={exportData4}>
+        //     <li className="download-image"> </li>{" "}
+        //   </button>
+        // </ul>
+        // <ul className="Table-datas">
+        //   <li>{DataName5}</li>
+        //    <button className="downloadButton" onClick={exportData5}>
+        //     <li className="download-image"> </li>{" "}
+        //   </button>
+        // </ul>
+        // <ul className="Table-datas">
+        //   <li>{DataName6}</li>
+        //   <button className="downloadButton" onClick={exportData6}>
+        //     <li className="download-image"> </li>{" "}
+        //   </button>
+        // </ul>
+        // <ul className="Table-datas">
+        //   <li>{DataName7}</li>
+        //    <button className="downloadButton" onClick={exportData7}>
+        //     <li className="download-image"> </li>{" "}
+        //   </button>
+        // </ul>
+        // <ul className="Table-datas">
+        //   <li>{DataName8}</li>
+        //    <button className="downloadButton" onClick={exportData8}>
+        //     <li className="download-image"> </li>{" "}
+        //   </button>
+        // </ul> */}
       </div>
     </div>
   );
