9c9666e0e46c5598754b167402221bf68d6433d0
EnsarBatuhanAyd <ensarbatuhanayd@gmail.com>
2022-08-19 11:14:22 +0300

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Picoampermeter-realtime-software/",
-  plugins: [react()]
+   plugins: [react()],
+   base: '/Picoampermeter-realtime-software/'
 })
